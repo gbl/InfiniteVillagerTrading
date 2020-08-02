fbe33c84ae9ac0377020a02ab073938b1e66f7a7
Guntram Blohm <gbl@guntram.de>
2020-08-02 18:13:29 +0200

    suppress a warning message from the loader

--- src/main/java/de/guntram/mcmod/infinitevillagertrading/mixins/MixinTradeOffer.java
+++ src/main/java/de/guntram/mcmod/infinitevillagertrading/mixins/MixinTradeOffer.java
@@ -3,6 +3,7 @@ package de.guntram.mcmod.infinitevillagertrading.mixins;
 import net.minecraft.village.TradeOffer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TradeOffer.class) 
 public class MixinTradeOffer {
     
-    @Shadow private @Final int maxUses;
+    @Shadow private @Final @Mutable int maxUses;
     
     @Inject(method = "<init>*", at = @At("RETURN"))
     public void forceHighMaxUseCount(CallbackInfo ci) {
